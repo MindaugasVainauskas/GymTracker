b00085bdbb83ebb7a9dbd348dbf776e0487ece55
M.Vainauskas <mindelis1@gmail.com>
2017-11-30 17:14:33 +0000

    Added folders for mvvm framework and basic view page for where list will later be shown

---
+++ GymTrackerApp/GymTrackerApp/Model/GymSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTrackerApp.Model
+{
+    class GymSession
+    {
+        public string exercise;
+        public int sets;
+        public int reps;
+    }
+}
---
+++ GymTrackerApp/GymTrackerApp/ViewModel/SessionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GymTrackerApp.Model;
+
+namespace GymTrackerApp.ViewModel
+{
+    class SessionViewModel
+    {
+        SessionDate session;
+        public List<GymSession> exercises;
+    }
+}
